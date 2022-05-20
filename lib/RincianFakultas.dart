6f8bd521e909291a2ccd88a80894b600d885f210
alyachfa <75361865+alyachfa@users.noreply.github.com>
2022-05-20 17:45:01 +0700

    Update RincianFakultas.dart
    menambahkan gambar pada rincian fakultas

--- lib/RincianFakultas.dart
+++ lib/RincianFakultas.dart
@@ -9,7 +9,11 @@ class RincianFakultas extends StatelessWidget {
         title: const Text('Rincian Fakultas'),
       ),
       body: Center(
-        child: Text("rincian fakultas"),
+        child: Image.network(
+          "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+          fit: BoxFit.cover,
+          height: 200,
+        ),
       ),
     );
   }
